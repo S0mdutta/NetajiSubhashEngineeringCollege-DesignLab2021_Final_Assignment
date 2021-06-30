4e75d2c4f6f350cfa31b5f528c0dcaefb912cad5
Somdutta Roy <somduttaroy2510@gmail.com>
2021-06-30 22:12:57 +0530

    working state

---
+++ api/data.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 01,
+    imageUrl:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTNcsXaVf-opI2t5IIikl0T9-0GYnKV6mgIUIz1TQ782VD5yc3wHSS0o4h1pYvdLE6htX7PqVXPCdhufZTo8VOwpYmKYrSaYDEspPsCo-4&usqp=CAE',
+    name: 'Men Regular Fit Checkered Mandarin Collar Casual Shirt',
+    available_quantity: 5,
+    price: 450,
+    description: 'Black and green checked casual shirt, has a mandarin collar, button placket, long sleeves, curved hemKeep warm on the coldest of days with this top-of-the-line shirt by Roadster. A pair of sneakers and slim denim are the perfect additions to this black piece when you are going outside for a day in the park.'
+  },
+
+  {
+    id: 02,
+    imageUrl:'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR2ZHUVjUOILokYCDRsiUYC5zVSrzdqYL7xZFkY7pxwmjFtti5tRKc6MQB4_u2QAb0RxYmG6VwvpdjS_c4rsvBrIC05Smjb6XD4AWZSD-vpSDq11KUstSxM&usqp=CAE',
+    name: 'U.S. Polo Assn. Grey Melange Full Sleeves T-Shirt',
+    available_quantity: 7,
+    price: 50,
+    description: 'Add this well-designed T-shirt from Roadster to your wardrobe and stay warm all year long. When you need a laid-back weekend ensemble, rock this blue piece with trim denims and sneakers.Grey printed casual shirt, has a spread collar, button placket, 1 pocket, long sleeves, curved hem'
+  },
+
+  {
+    id: 03,
+    imageUrl:'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRi-u48hW5nSCT142KYlqCWfNrtynFQSLmtlW4uOw09jiyzflt7MIyuYgOE8juGZr_4r2BrY9TTYCg_htuuD7THaO3tJ0Vny0TpoSbeN0riI3vrC_poP5bOIA&usqp=CAE',
+    name: 'NG38003PP13J Tees Analog Watch - For Men & Women',
+    available_quantity: 0,
+    price: 500,
+    description: 'Analog Wrist Watch Strap Width - 22 mm, Strap Length - 195 mm, Dial Diameter - 39 mm.This is a genuine Fastrack product. The product comes with a standard manufacturer warranty for 6 Months.'
+  },
+
+  {
+    id: 04,
+    imageUrl:'https://rukminim1.flixcart.com/image/800/960/klphn680/shoe/c/n/l/10-rbb791-robbie-jones-black-original-imagyrm8p8jakace.jpeg?q=50',
+    name: 'Casual Sneakers Black Shoes For Men Sneakers For Men  (Black)',
+    available_quantity: 4,
+    price: 1500,
+    description: 'Robbie Jones Shoes Are Designed To Keeping In Mind Durability As Well As Trends, The Most Stylish Range Of Shoes. They Are Exclusively Designed To Match The Latest Trends Of The New Generation. This Pair Of Shoes Is Sure To Make You Look Smart & Classy. These Will Go With Most Of Your Casual Outfits. This Product Is Made Of Premium Quality And Highly Material. Wearing This Prime Quality Amazing And Cozy Shoe With The Combination Of Jean And T-Shirt. It Presents Standard Look To Your Personality. We Are Always Providing Latest Fashion Shoes For Mens Casual Shoes For Men.'
+  },
+];
+
+
+
+const users = [
+   {
+      'name': 'user',
+      'password': 'qwerty'
+    },
+    {
+      'name': 'example',
+      'password': 'qwerty'
+    }
+];
+
+module.exports = { 'products': products, users: users }
---
+++ src/repository.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:5000';
+
+export function getProducts() {
+	return axios.get(`${BASE_URL}/api/products`)
+		.then(response => response.data);
+}
+
+export function getCartProducts(cart) {
+	return axios.post(`${BASE_URL}/api/products`, {cart})
+		.then(response => response.data);
+}
+
+
+
+export function isAuthenticated(){
+	return localStorage.getItem('x-access-token') && localStorage.getItem('x-access-token-expiration') > Date.now()
+}
